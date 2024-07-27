72069104837137fcc93b5f73ba63df18daef841d
Iyappan97 <154623253+Iyappan97@users.noreply.github.com>
2024-07-27 10:57:08 +0530

    Update kubefile.tf

--- kubenetes/kubefile.tf
+++ kubenetes/kubefile.tf
@@ -16,7 +16,7 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
-    bucket = "backenedteraform"
+    bucket = "backenedteraform1"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
